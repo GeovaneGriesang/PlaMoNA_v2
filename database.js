ec58956fd2d53a8edfb9991bd6e34da268bb4af2
cocacolacomvodka <joaogabrielde.almeida@hotmail.com>
2023-09-27 22:09:34 -0300

    .env vars

--- database.js
+++ database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: '',
-  database: 'plamona',
+  host: process.env.BD_HOST,
+  port: process.env.BD_PORT,
+  user: process.env.BD_USER,
+  password: process.env.BD_PASS,
+  database: process.env.BD_DATA,
 });
 
 connection.connect((error) => {
